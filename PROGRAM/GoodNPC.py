b0b238b1da836bcc79438b54b9ac365d001c5caa
tatxha <134766457+tatxha@users.noreply.github.com>
2024-03-07 23:39:32 +0700

    GoodNPC.py

--- PROGRAM/GoodNPC.py
+++ PROGRAM/GoodNPC.py
@@ -1,4 +1,4 @@
-#  Class Good NPC merupakan child dari Class Character juga, Class ini memiliki informasi mengenai data Player, data misi, dan item yang dibeli dari Player
+# Class Player merupan child dari Class Character dan Class Player memiliki item dari class item
 from Character import Character 
 from Player import Player
 from Missions import Missions 
@@ -6,22 +6,21 @@ from Items import Items
 
 class GoodNPC(Character):
     # private attributes
+    __items_bought = []
     __missions = []
-    __items = []
     
-    # constructor
+    # cosntructor
     def __init__(self, name= "", gender = '', id = ""):
         super().__init__(name, gender, id)
-        self.__items = []
+        self.__items_bought = []
         self.__missions = []
     
     # getter
-    def get_items(self): return self.__items
     def get_mission(self): return self.__missions
     
     # methods
     def add_mission(self, mission : Missions):  # Metode untuk menambahkan misi
-        self.__missions.append(mission)             
+        self.__missions.append(mission)   
     def assign_mission_to_player(self, player, selected_mission):
         player.add_mission(selected_mission)
     def display_player_information(self, player : Player):
@@ -31,7 +30,7 @@ class GoodNPC(Character):
         print("| HP         :", player.get_hp())
         print("| Skill      :", player.get_skill())
         print("| ATK        :", player.get_atk())
-        print("| Coin       : $", player.get_coin())
+        print("| Coin       :", player.get_coin())
         print("| Key        :", player.get_key())
         print("+------------------------------------------------+")
     def buying_item(self, bought):
