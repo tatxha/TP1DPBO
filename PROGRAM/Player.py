8f69caa48bf12d99b994504c926415858a38ad02
tatxha <134766457+tatxha@users.noreply.github.com>
2024-03-07 23:31:07 +0700

    Player.py

--- PROGRAM/Player.py
+++ PROGRAM/Player.py
@@ -1,4 +1,4 @@
-# Class Player merupan child dari Class PlayableCharacter dan Class Player memiliki items dari Class Items
+# Class Player merupan child dari Class Character dan Class Player memiliki item dari class item
 from PlayableCharacter import PlayableCharacter
 from Items import Items
 
@@ -9,6 +9,8 @@ class Player(PlayableCharacter):
     __coin = 0
     __key = 0
     __items = []
+    __missions = []
+    
     
     # constructor
     def __init__(self, id = "", name = "", gender = '', hp = 0, atk = 0, skill = "", exp = 0, coin = 0, key = 0):
@@ -18,8 +20,9 @@ class Player(PlayableCharacter):
         self.__coin = coin
         self.__key = key
         self.__items = []
-      
-    # setter dan getter  
+        self.__missions = []
+        
+    # setter dan getter
     def set_skill(self, skill): self.__skill = skill
     def set_key(self, key): self.__key = key
     def set_coin(self, coin): self.__coin = coin
@@ -27,11 +30,14 @@ class Player(PlayableCharacter):
     
     def get_skill(self): return self.__skill
     def get_key(self): return self.__key
+    def get_missions(self): return self.__missions
     def get_items(self): return self.__items
     def get_coin(self): return self.__coin
     def get_exp(self): return self.__exp
     
     # methods
+    def add_mission(self, mission):
+        self.__missions.append(mission)
     def add_item(self, item  : Items):
         self.__items.append(item)
     def add_coin(self, reward): 
@@ -53,7 +59,3 @@ class Player(PlayableCharacter):
         print("---------------------------------------------")
     def sell_item(self, npc, selected_item):
         npc.buying_item(selected_item)
-    
-    
-
-        
